66595442d3c835e9ab07c0ee6e730fe170d96ba2
Clementee <61148897+Clementee@users.noreply.github.com>
2020-02-17 14:45:55 +0100

    Complete tree

--- src/ch/epfl/rigel/Preconditions.java
+++ src/ch/epfl/rigel/Preconditions.java
@@ -3,12 +3,16 @@ package ch.epfl.rigel;
 public final class Preconditions {
     private Preconditions() {
     }
+    
+        // public method checking if the argument selected is valid or not and if not, throwing an exception
         public static void checkArgument (boolean isTrue) {
+            
             if(!isTrue) {
                 throw new IllegalArgumentException();
             }
         }
         
+     // public method checking if the value chosen is valid in the interval studied. If not throwing exception, else throw the value
      public static double checkInterval (Interval interval, double value) {
           
           double lowerBound;
