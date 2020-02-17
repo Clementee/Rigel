8c0b509fd30651f16c2c078e5ddeae1d94435f1c
Clementee <61148897+Clementee@users.noreply.github.com>
2020-02-17 14:30:24 +0100

    allo

--- src/ch/epfl/rigel/Preconditions.java
+++ src/ch/epfl/rigel/Preconditions.java
@@ -3,9 +3,28 @@ package ch.epfl.rigel;
 public final class Preconditions {
     private Preconditions() {
     }
-        public static void checkArgument (boolean isTrue); 
-        public static double checkInterval (Interval interval, double value);
+        public static void checkArgument (boolean isTrue) {
+            if(!isTrue) {
+                throw new IllegalArgumentException();
+            }
+        }
         
-    
-
+     public static double checkInterval (Interval interval, double value) {
+          
+          double lowerBound;
+          double upperBound;
+            
+            
+          lowerBound = interval.low();
+          upperBound = interval.high();
+            
+            if(lowerBound <= value <= upperBound) {
+                return value;
+            }
+            
+            else {
+                throw new IllegalArgumentException();
+            }
+        }
+        
 }
