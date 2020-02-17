13960c97877a13019b02ce7896604400e358edb2
Blecoeur <56965256+Blecoeur@users.noreply.github.com>
2020-02-17 14:10:15 +0100

    first commit

---
+++ src/ch/epfl/rigel/Preconditions.java
@@ -0,0 +1,11 @@
+package ch.epfl.rigel;
+
+public final class Preconditions {
+    private Preconditions() {
+    }
+        public static void checkArgument (boolean isTrue); 
+        public static double checkInterval (Interval interval, double value);
+        
+    
+
+}
