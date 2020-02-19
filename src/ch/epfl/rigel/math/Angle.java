6457cbd6ea7365da1bffdac70eb8826af8101164
Blecoeur <56965256+Blecoeur@users.noreply.github.com>
2020-02-19 21:26:28 +0100

    Create Angle.java

---
+++ src/ch/epfl/rigel/math/Angle.java
@@ -0,0 +1,100 @@
+package ch.epfl.rigel.math;
+
+//non instanciable == abstract?
+//test effectué
+public final class Angle {
+    public final static double TAU = 2 * Math.PI;
+    private final static int SEC_PER_DEG = 360;
+    private final static int MIN_TO_SEC = 60;
+    private final static double RAD_PER_HOUR = TAU / 24;
+
+    /**
+     * Method normalizing the angle
+     * 
+     * @param rad
+     *            (double) : the angle in radians
+     * @return (double) : the angle in radians belonging to the interval
+     *         [0,2*pi]
+     */
+    public static double normalizePositive(double rad) {
+        return rad % TAU;
+    }
+
+    /**
+     * Method allowing to convert an angle from seconds to radians(and normalize
+     * it)
+     * 
+     * @param sec
+     *            (double) : the angle in seconds
+     * @return (double) : the normalized angle in radians
+     */
+    public static double ofArcsec(double sec) {
+        return Math.toRadians(sec / SEC_PER_DEG);
+    }
+
+    /**
+     * Method converting an angle in degrees, minutes and seconds to an angle in
+     * radians (and normalize it). If the measures are not conform, an
+     * IllergalArgumentException is thrown
+     * 
+     * @param deg
+     *            (int) : the number of degrees
+     * @param min
+     *            (int) : the number of minutes in the interval [0,60[
+     * @param sec
+     *            (double) : the number of seconds in the interval [0,60[
+     * @return (double) : the normalized angle in radians
+     */
+    public static double ofDMS(int deg, int min, double sec) {
+        if (min <= 0 || min > 60 || sec <= 0 || sec > 60) {
+            throw new IllegalArgumentException();
+        } else {
+            double angle = deg + ofArcsec(min * MIN_TO_SEC + sec);
+            return Math.toRadians(angle);
+        }
+    }
+
+    /**
+     * Method converting an angle in degrees to a normalized angle in radians
+     * 
+     * @param deg
+     *            (double) : the angle in degrees
+     * @return (double) : the angle in radians
+     */
+    public static double ofDeg(double deg) {
+        return Math.toRadians(deg);
+    }
+
+    /**
+     * Method converting an angle in radians to a normalized angle in degrees
+     * 
+     * @param rad
+     *            (double) : the angle in radians
+     * @return (double) : the normalized angle in degrees.
+     */
+    public static double toDeg(double rad) {
+        return Math.toDegrees(rad);
+    }
+
+    /**
+     * Method converting an angle in hour to a normalized angle in radians
+     * 
+     * @param hr
+     *            (double) : the angle in hour
+     * @return (double) : the angle in radians
+     */
+    public static double ofHr(double hr) {
+        return hr * RAD_PER_HOUR;
+    }
+    
+    /**
+     * Method converting an angle in radians to a normalized angle in hours
+     * 
+     * @param rad
+     *            (double) : the angle in radians
+     * @return (double ) : the normalized angle in hours
+     */
+    public static double toHr(double rad) {
+        return rad / RAD_PER_HOUR;
+    }
+}
