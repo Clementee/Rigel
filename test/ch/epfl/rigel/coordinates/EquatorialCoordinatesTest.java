97464e9e6b58e9e72f15a6914401f49453b41dc7
Clementee <61148897+Clementee@users.noreply.github.com>
2020-02-21 14:41:48 +0100

    Update EquatorialCoordinatesTest.java

--- test/ch/epfl/rigel/coordinates/EquatorialCoordinatesTest.java
+++ test/ch/epfl/rigel/coordinates/EquatorialCoordinatesTest.java
@@ -1,49 +1,46 @@
-package ch.epfl.rigelTest.coordinates;
+package ch.epfl.rigel.coordinates;
 
-import ch.epfl.rigel.coordinates.EquatorialCoordinates;
 import ch.epfl.rigel.math.Angle;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class EquatorialCoordinatesTest {
 
-    EquatorialCoordinates equatorialCoordinates = EquatorialCoordinates.of(1.5333, 45);
-
-
+    private static final double EPSILON = 1e-3;
+    EquatorialCoordinates equatorialCoordinates = EquatorialCoordinates.of(Angle.ofHr(1.5333), Angle.ofDeg(45));
 
     @Test
-    void ra() {
-        assertEquals(0.40141700131243585, equatorialCoordinates.ra());
+    void raWorksWithValidValues() {
+        assertEquals(0.401417, equatorialCoordinates.ra(), EPSILON);
 
     }
 
     @Test
-    void raDeg() {
-        assertTrue(Math.abs(((23) - equatorialCoordinates.raDeg())) <= 0.001);
+    void raDegWorksWithValidValues() {
+        assertEquals(23,equatorialCoordinates.raDeg(),EPSILON);
 
     }
 
     @Test
-    void raHr() {
-        assertTrue(Math.abs(Angle.toHr(Angle.ofDeg(23)) - equatorialCoordinates.raHr()) <= 0.001 );
+    void raHrWorksWithValidValues() {
+        assertEquals(Angle.toHr(Angle.ofDeg(23)), equatorialCoordinates.raHr(), EPSILON);
 
     }
 
     @Test
-    void dec() {
+    void decWorksWithValidValues() {
         assertEquals(Angle.ofDeg(45), equatorialCoordinates.dec());
 
     }
 
     @Test
-    void decDeg() {
+    void decDegWorksWithValidValues() {
         assertEquals((45), equatorialCoordinates.decDeg());
 
     }
 
     @Test
-    void testToString() {
-            assertEquals("(ra=1.5333h, dec=45.0000°)", equatorialCoordinates.toString());
+    void testToStringWorksWithValidValues() {
+        assertEquals("(ra=1.5333h, dec=45.0000°)", equatorialCoordinates.toString());
     }
-}
