94690141fc1b2ef39ed6e8fc7e4130beb30666ce
Clementee <61148897+Clementee@users.noreply.github.com>
2020-02-25 21:42:54 +0100

    Update Epoch.java

--- src/ch/epfl/rigel/astronomy/Epoch.java
+++ src/ch/epfl/rigel/astronomy/Epoch.java
@@ -2,9 +2,6 @@ package ch.epfl.rigel.astronomy;
 
 import java.time.*;
 import java.time.temporal.ChronoUnit;
-import java.time.temporal.Temporal;
-
-import static java.time.temporal.ChronoUnit.MILLIS;
 
 
 public enum Epoch {
@@ -12,7 +9,6 @@ public enum Epoch {
     J2000(LocalDate.of(2000, Month.JANUARY,1),LocalTime.of(12,0), ZoneOffset.UTC),
     J2010(LocalDate.of(2010, Month.JANUARY, 1).minusDays(1),LocalTime.of(0,0),ZoneOffset.UTC);
 
-    private static double deltaDays;
     private ZonedDateTime thisZoneTime;
 
     Epoch(LocalDate currentDate, LocalTime currentHour, ZoneId zone) {
@@ -20,15 +16,14 @@ public enum Epoch {
     }
 
     public double daysUntil(ZonedDateTime when){
-        deltaDays = -when.until(thisZoneTime, ChronoUnit.MILLIS);
+        double deltaDays = -when.until(thisZoneTime, ChronoUnit.MILLIS);
         deltaDays = deltaDays/1000;
         deltaDays = deltaDays/3600;
         deltaDays = deltaDays/24;
         return deltaDays;
     }
 
-    public static double julianCenturiesUntil(ZonedDateTime when){
-        return deltaDays/36525;
+    public double julianCenturiesUntil(ZonedDateTime when){
+        return daysUntil(when)/36525;
     }
 }
-
