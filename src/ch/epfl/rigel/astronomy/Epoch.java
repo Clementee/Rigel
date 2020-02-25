a156ce0633c514cbadcd5cfb5fe24f040868a477
Clementee <61148897+Clementee@users.noreply.github.com>
2020-02-25 14:37:47 +0100

    Update Epoch.java

--- src/ch/epfl/rigel/astronomy/Epoch.java
+++ src/ch/epfl/rigel/astronomy/Epoch.java
@@ -1,33 +1,34 @@
 package ch.epfl.rigel.astronomy;
 
 import java.time.*;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
 
 import static java.time.temporal.ChronoUnit.MILLIS;
+
 
 public enum Epoch {
 
     J2000(LocalDate.of(2000, Month.JANUARY,1),LocalTime.of(12,0), ZoneOffset.UTC),
     J2010(LocalDate.of(2010, Month.JANUARY, 1).minusDays(1),LocalTime.of(0,0),ZoneOffset.UTC);
 
-     private double deltaDays;
-     public ZonedDateTime thisZoneTime;
-
+    private static double deltaDays;
+    private ZonedDateTime thisZoneTime;
 
     Epoch(LocalDate currentDate, LocalTime currentHour, ZoneId zone) {
-        thisZoneTime = ZonedDateTime.of(currentDate,currentHour,zone);
+        thisZoneTime = ZonedDateTime.of(currentDate, currentHour, zone);
     }
 
     public double daysUntil(ZonedDateTime when){
-        
-        deltaDays = when.until(thisZoneTime, ChronoUnit.MILLIS);
-        deltaDays = deltaDays*1000;
-        deltaDays = deltaDays*3600;
-        deltaDays = deltaDays*24;
+        deltaDays = -when.until(thisZoneTime, ChronoUnit.MILLIS);
+        deltaDays = deltaDays/1000;
+        deltaDays = deltaDays/3600;
+        deltaDays = deltaDays/24;
         return deltaDays;
     }
 
-    public double julianCenturiesUntil(ZonedDateTime when){
+    public static double julianCenturiesUntil(ZonedDateTime when){
         return deltaDays/36525;
     }
-
 }
+
