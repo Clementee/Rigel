4515882867084a280f74fb91d91404912fce3575
Clementee <61148897+Clementee@users.noreply.github.com>
2020-02-27 17:37:31 +0100

    Update Epoch.java

--- src/ch/epfl/rigel/astronomy/Epoch.java
+++ src/ch/epfl/rigel/astronomy/Epoch.java
@@ -5,16 +5,28 @@ import java.time.temporal.ChronoUnit;
 
 
 public enum Epoch {
-
+    
+    //values of the enumeration with its characteristics
     J2000(LocalDate.of(2000, Month.JANUARY,1),LocalTime.of(12,0), ZoneOffset.UTC),
     J2010(LocalDate.of(2010, Month.JANUARY, 1).minusDays(1),LocalTime.of(0,0),ZoneOffset.UTC);
 
     public ZonedDateTime thisZoneTime;
 
+    /**
+     * Epoch package-private constructor
+     * @param currentDate     (LocalTime) : gives the date of the specific term of the enumeration
+     * @param currentHour     (LocalTime) : gives the hour of the specific term of the enumeration
+     * @param zone            (ZoneId) : gives the name of the time zone for the date
+     */
     Epoch(LocalDate currentDate, LocalTime currentHour, ZoneId zone) {
         thisZoneTime = ZonedDateTime.of(currentDate, currentHour, zone);
     }
 
+    /**
+     * Public method returning the number of days between the parameter value and a settled time in the enumeration
+     * @param when       (ZonedDateTime) : select the zoned date time 
+     * @return deltaDays (double) : return the difference of days
+     */
     public double daysUntil(ZonedDateTime when){
         double deltaDays = -when.until(thisZoneTime, ChronoUnit.MILLIS);
         deltaDays = deltaDays/1000;
@@ -23,6 +35,11 @@ public enum Epoch {
         return deltaDays;
     }
 
+    /**
+     * Public method returning the number of julian centuries between the parameter value and a settled time in the enumeration
+     * @param when       (ZonedDateTime) : select the zoned date time 
+     * @return julianCenturies (double) : return the number of julian centuries between the two dates
+     */
     public double julianCenturiesUntil(ZonedDateTime when){
         return daysUntil(when)/36525;
     }
