37130f41e75b42c129827bdf85352ab597c25328
Clementee <61148897+Clementee@users.noreply.github.com>
2020-02-27 18:10:12 +0100

    Completed

--- src/ch/epfl/rigel/coordinates/SphericalCoordinates.java
+++ src/ch/epfl/rigel/coordinates/SphericalCoordinates.java
@@ -2,6 +2,12 @@ package ch.epfl.rigel.coordinates;
 
 import ch.epfl.rigel.math.Angle;
 
+/**
+ * Spherical coordinates
+ *
+ * @author Baptiste Lecoeur (316223)
+ * @author Clement Sanh (311427)
+ */
 abstract class SphericalCoordinates {
     
     private double longitudeCoords;
@@ -17,33 +23,40 @@ abstract class SphericalCoordinates {
         latitudeCoords = latitude;
     }
     
-    double lon() {
-        return longitudeCoords;
-    }
+    /**
+     * Public method used to return the value of the longitude in radians
+     * 
+     * @return :    return value of longitude in radians
+     */
+    double lon() { return longitudeCoords;}
     
-    double lonDeg() {
-        return Angle.toDeg(longitudeCoords);
-    }
+    /**
+     * Public method used to return the value of the longitude in degrees
+     * 
+     * @return :    return value of longitude in degrees
+     */
+    double lonDeg() { return Angle.toDeg(longitudeCoords);}
     
-    double lat() {
-        return latitudeCoords;
-    }
+    /**
+     * Public method used to return the value of the latitude in radians
+     * 
+     * @return :    return value of latitude in radians
+     */
+    double lat() { return latitudeCoords;}
     
-    double latDeg() {
-        System.out.println(latitudeCoords);
-        return Angle.toDeg(latitudeCoords);
-    }
+    /**
+     * Public method used to return the value of the latitude in degrees
+     * 
+     * @return :    return value of latitude in degrees
+     */
+    double latDeg() { return Angle.toDeg(latitudeCoords);}
     
     //Method overridden in order to throw an exception when called
     @Override
-    public final int hashCode() {
-        throw new UnsupportedOperationException();
-    }
+    public final int hashCode() { throw new UnsupportedOperationException();}
     
     //Method overridden in order to throw an exception when called
     @Override
-    public final boolean equals(Object obj) {
-        throw new UnsupportedOperationException();
-    }
+    public final boolean equals(Object obj) { throw new UnsupportedOperationException();}
 
 }
