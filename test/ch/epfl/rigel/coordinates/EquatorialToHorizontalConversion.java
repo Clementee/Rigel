5f19d423e92b7dc336b5327c9f08c8127a8ac1d2
Clementee <61148897+Clementee@users.noreply.github.com>
2020-03-02 18:09:17 +0100

    Create EquatorialToHorizontalConversion.java

---
+++ test/ch/epfl/rigel/coordinates/EquatorialToHorizontalConversion.java
@@ -0,0 +1,27 @@
+package ch.epfl.rigel.coordinates;
+
+import ch.epfl.rigel.math.Angle;
+import org.junit.jupiter.api.Test;
+
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EquatorialToHorizontalConversionTest {
+    EquatorialToHorizontalConversion convert = new
+            EquatorialToHorizontalConversion(ZonedDateTime.of(2009,7,6,0,0,0,0, ZoneOffset.UTC),
+            GeographicCoordinates.ofDeg(-135.352088857, 52));
+    //-135.352088857
+    @Test
+    void applyLatWorks() {
+        assertEquals(Angle.ofDeg(19.334345), convert.apply(EquatorialCoordinates.of(Angle.ofDMS(5 * 360 / 24, 51, 44),
+                Angle.ofDMS(23, 13, 10))).lat());
+    }
+
+    @Test
+    void applyLonWorks() {
+        assertEquals(Angle.ofDeg(283.271027), convert.apply(EquatorialCoordinates.of(Angle.ofDMS(5 * 360 / 24, 51, 44),
+                Angle.ofDMS(23, 13, 10))).lon());
+    }
+}
