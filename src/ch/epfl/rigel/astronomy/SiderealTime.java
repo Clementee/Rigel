660e0d5d43408b40a08d806c09002e77709a617e
Clementee <61148897+Clementee@users.noreply.github.com>
2020-03-03 22:52:41 +0100

    Update SiderealTime.java

--- src/ch/epfl/rigel/astronomy/SiderealTime.java
+++ src/ch/epfl/rigel/astronomy/SiderealTime.java
@@ -18,11 +18,15 @@ import static ch.epfl.rigel.math.Angle.*;
  * @author Clement Sanh (311427)
  */
 public final class SiderealTime {
+    
     private final static double MILLITOSECONDS = 1000;
     private final static double SECONDSTOHOURS = 3600;
     private final static double S1_COEFF = 1.002737909;
     private final static Polynomial S0POLY = Polynomial.of(0.000025862,2400.051336,6.697374558);
 
+   /**
+     * Sidereal Time empty private constructor
+     */
     private SiderealTime(){}
 
     /**
@@ -32,15 +36,18 @@ public final class SiderealTime {
      * @return (double) : the Sidereal Greenwich time in radians(in the interval [0, PI[
      */
     public static double greenwich(ZonedDateTime when){
+        
         ZonedDateTime whenBis = when.withZoneSameInstant(ZoneId.of("UTC"));
         double T =Epoch.J2000.julianCenturiesUntil(whenBis.truncatedTo(ChronoUnit.DAYS));
         double t = whenBis.truncatedTo(ChronoUnit.DAYS).until(whenBis, ChronoUnit.MILLIS)/(MILLITOSECONDS*SECONDSTOHOURS);
         double S0 = S0POLY.at(T);
         double S1 = S1_COEFF*t;
         double Sg = S0+S1;
+        
         if(Sg<0){
             Sg=Sg % 24+24;
-        }else{
+        }
+        else{
             Sg=Sg%24;
         }
         return ofHr(Sg);
@@ -58,5 +65,4 @@ public final class SiderealTime {
         double λ = ofDeg(where.lonDeg());
         return Sg + λ; 
     }
-
 }
