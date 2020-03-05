efa19dd65ea234bc3af837efbc1dca90e598054b
Clementee <61148897+Clementee@users.noreply.github.com>
2020-03-05 17:25:21 +0100

    Update EquatorialToHorizontalConversionTest.java

--- test/ch/epfl/rigel/coordinates/EquatorialToHorizontalConversionTest.java
+++ test/ch/epfl/rigel/coordinates/EquatorialToHorizontalConversionTest.java
@@ -1,5 +1,8 @@
-package ch.epfl.rigel.coordinates;
+package ch.epfl.coordinates;
 
+import ch.epfl.rigel.coordinates.EquatorialCoordinates;
+import ch.epfl.rigel.coordinates.EquatorialToHorizontalConversion;
+import ch.epfl.rigel.coordinates.GeographicCoordinates;
 import ch.epfl.rigel.math.Angle;
 import org.junit.jupiter.api.Test;
 
@@ -9,19 +12,24 @@ import java.time.ZonedDateTime;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EquatorialToHorizontalConversionTest {
+
+    private final static double EPSILON = 1e-6;
+
     EquatorialToHorizontalConversion convert = new
             EquatorialToHorizontalConversion(ZonedDateTime.of(2009,7,6,0,0,0,0, ZoneOffset.UTC),
             GeographicCoordinates.ofDeg(-135.352088857, 52));
     //-135.352088857
+
+
     @Test
     void applyLatWorks() {
-        assertEquals(Angle.ofDeg(19.334345), convert.apply(EquatorialCoordinates.of(Angle.ofDMS(5 * 15, 51, 44),
-                Angle.ofDMS(23, 13, 10))).lat());
+        assertEquals(Angle.ofDeg(19.334345), convert.apply(EquatorialCoordinates.of(convert.siderealTime-Angle.ofHr(5.862222),
+                Angle.ofDeg(23.219444))).alt(),EPSILON);
     }
 
     @Test
     void applyLonWorks() {
-        assertEquals(Angle.ofDeg(283.271027), convert.apply(EquatorialCoordinates.of(Angle.ofDMS(5 * 15, 51, 44),
-                Angle.ofDMS(23, 13, 10))).lon());
+        assertEquals(Angle.ofDeg(283.271027), convert.apply(EquatorialCoordinates.of(convert.siderealTime-Angle.ofHr(5.862222),
+                Angle.ofDeg(23.219444))).az(),EPSILON);
     }
 }
