0a4e444a13f9ddc545054fbe380116107ac72ec6
Clementee <61148897+Clementee@users.noreply.github.com>
2020-03-09 16:08:20 +0100

    Update Moon.java

--- src/ch/epfl/rigel/astronomy/Moon.java
+++ src/ch/epfl/rigel/astronomy/Moon.java
@@ -5,7 +5,6 @@ import ch.epfl.rigel.math.ClosedInterval;
 
 import java.util.Locale;
 
-
 /**
  * The moon
  *
@@ -22,24 +21,23 @@ public final class Moon extends CelestialObject {
          * @param equatorialPos   (EquatorialCoordinates) : gives the equatorial coordinates of the planet
          * @param angularSize     (float) : gives the angular size of the planet
          * @param magnitude       (float) : gives the magnitude of the planet
-         * @param phase           (float) : gives the phase 
+         * @param phase           (float) : gives the phase
          */
-    public Moon(EquatorialCoordinates equatorialPos, float angularSize, float magnitude, float phase) {
+        public Moon(EquatorialCoordinates equatorialPos, float angularSize, float magnitude, float phase) {
 
         super(moonName, equatorialPos, angularSize, magnitude);
 
         if (!(ClosedInterval.of(0, 1).contains(phase))) {
             throw new IllegalArgumentException();
         }
+
+        else {
+            moonPhase = phase*100;
+        }
     }
-    
-    /**
-     * Moon method returning a string with the informations around the moon
-     *
-     * @return stringMoon (String) : returining the name of the moon with its phase
-     */
+
     @Override
     public String info(){
-        return moonName + " (" +String.format(Locale.ROOT,"(%.1f)",moonPhase*100)+ "%)";
+        return moonName + " (" +String.format(Locale.ROOT,"%.1f",moonPhase)+ "%)";
     }
 }
