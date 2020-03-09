e8b7bf88d89c3369bc921d459d343bcbb3805d66
Clementee <61148897+Clementee@users.noreply.github.com>
2020-03-09 14:14:03 +0100

    Create Asterism.java

---
+++ src/ch/epfl/rigel/astronomy/Asterism.java
@@ -0,0 +1,38 @@
+package ch.epfl.rigel.astronomy;
+
+import java.util.List;
+
+
+/**
+ * An asterism
+ *
+ * @author Baptiste Lecoeur (316223)
+ * @author Clement Sanh (311427)
+ */
+public final class Asterism {
+
+    private List<Star> asterismList;
+
+    /**
+     * Asterism package-private constructor initializing the asterism with the stars composing it
+     * @param stars   (List<Star>) : gives a list of stars present in the asterism
+     */
+    Asterism(List<Star> stars){
+        
+        if(!stars.isEmpty()){
+            asterismList = stars;
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * Asterism method returning the list of stars composing the asterism
+     * 
+     * @return asterismList (List<Star>) : return the list of stars composing the astersim
+     */
+    public List<Star> stars(){
+        return asterismList;
+    }
+}
