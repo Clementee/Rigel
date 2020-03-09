1926c683251b2a055c8212e6fabcb9689e6d6a4e
Clementee <61148897+Clementee@users.noreply.github.com>
2020-03-09 16:53:34 +0100

    Update Sun.java

--- src/ch/epfl/rigel/astronomy/Sun.java
+++ src/ch/epfl/rigel/astronomy/Sun.java
@@ -5,10 +5,16 @@ import ch.epfl.rigel.coordinates.EquatorialCoordinates;
 
 import java.util.Objects;
 
+/**
+ * The sun
+ *
+ * @author Baptiste Lecoeur (316223)
+ * @author Clement Sanh (311427)
+ */
 public final class Sun extends CelestialObject{
     private final static String sunName = "Soleil";
     private static float sunPhase;
-    private final static float magnitude = 28.7f;
+    private final static float magnitude = -26.7f;
     private EclipticCoordinates eclipticPos;
     private float meanAnomaly;
 
