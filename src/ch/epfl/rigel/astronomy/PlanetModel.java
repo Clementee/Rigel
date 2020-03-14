b9c42a5c9d316976c8c385fc93d07dc6377730f5
Clementee <61148897+Clementee@users.noreply.github.com>
2020-03-14 15:45:23 +0100

    Update PlanetModel.java

--- src/ch/epfl/rigel/astronomy/PlanetModel.java
+++ src/ch/epfl/rigel/astronomy/PlanetModel.java
@@ -3,17 +3,19 @@ package ch.epfl.rigel.astronomy;
 import ch.epfl.rigel.coordinates.EclipticCoordinates;
 import ch.epfl.rigel.coordinates.EclipticToEquatorialConversion;
 import ch.epfl.rigel.coordinates.EquatorialCoordinates;
-import ch.epfl.rigel.math.Angle;
-
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
 import static ch.epfl.rigel.math.Angle.TAU;
 import static ch.epfl.rigel.math.Angle.ofDeg;
 import static java.lang.Math.*;
 
+/**
+ * Model of planet
+ *
+ * @author Baptiste Lecoeur (316223)
+ * @author Clement Sanh (311427)
+ */
 public enum PlanetModel implements CelestialObjectModel<Planet> {
     MERCURY("Mercure", 0.24085, 75.5671, 77.612, 0.205627,
             0.387098, 7.0051, 48.449, 6.74, -0.42),
@@ -37,39 +39,54 @@ public enum PlanetModel implements CelestialObjectModel<Planet> {
     private static Set<PlanetModel> innerPlanet = createInnerPlanetSet();
 
     private String frenchName;
-    private double Tp, epsilon, varpi,excentricity, a, i, omega, theta0, v0;
+    private double Tp, epsilon, varpi,eccentricity, a, i, omega, theta0, v0;
 
-    private PlanetModel(String frenchNameC, double TpC, double epsilonC, double varpiC, double excentricityC, double aC , double iC, double omegaC, double teta0C, double v0C){
+    /**
+     * PlanetModel private constructor initializing a planet with parameters
+     * @param frenchNameC     (String) : gives the french name of the planet
+     * @param TpC             (double) : gives the tropical year of the planet
+     * @param epsilonC        (double) : gives the longitude at J2010
+     * @param varpiC          (double) : gives the longitude at the perigee
+     * @param eccentricityC   (double) : gives the eccentricity of the orbit of the planet
+     * @param aC              (double) : gives the semi big axis of the orbit of the planet
+     * @param iC              (double) : gives the inclination of the orbit at the ecliptic
+     * @param omegaC          (double) : gives the longitude of the ascending node
+     * @param theta0C         (double) : gives the angular size of the planet
+     * @param v0C             (double) : gives the magnitude of the planet
+     */
+    private PlanetModel(String frenchNameC, double TpC, double epsilonC, double varpiC, double eccentricityC, double aC , double iC, double omegaC, double theta0C, double v0C){
         frenchName = frenchNameC;
         Tp = TpC;
         epsilon = ofDeg(epsilonC);
         varpi = ofDeg(varpiC);
-        excentricity = excentricityC;
+        eccentricity = eccentricityC;
         a = aC;
         i = ofDeg(iC);
         omega = ofDeg(omegaC);
-        theta0 = ofDeg(teta0C);
+        theta0 = ofDeg(theta0C);
         v0 = v0C;
-        }
+    }
 
     @Override
     public Planet at(double daysSinceJ2010, EclipticToEquatorialConversion eclipticToEquatorialConversion) {
         double M = TAU/365.242191 * daysSinceJ2010 / Tp + epsilon - varpi;
-        double v = M  + 2 * excentricity * sin(M);
-        double r = (a*(1-excentricity*excentricity))/(1+excentricity*Math.cos(v));
+        double v = M  + 2 * eccentricity * sin(M);
+        double r = (a*(1-eccentricity*eccentricity))/(1+eccentricity*Math.cos(v));
         double l = v  + varpi;
         double phi = Math.asin(sin(l - omega)* sin(i));
         double rPrim = r * cos(phi);
         double lPrim = Math.atan2(sin(l-omega)*cos(i), cos(l-omega))+omega;
         double MEarth = TAU/365.242191 * daysSinceJ2010 / EARTH.Tp + EARTH.epsilon - EARTH.varpi;
-        double vEarth = M  + 2 * EARTH.excentricity * sin(MEarth);
-        double rEarth = (EARTH.a*(1-EARTH.excentricity*EARTH.excentricity))/(1+EARTH.excentricity*Math.cos(vEarth));
+        double vEarth = M  + 2 * EARTH.eccentricity * sin(MEarth);
+        double rEarth = (EARTH.a*(1-EARTH.eccentricity*EARTH.eccentricity))/(1+EARTH.eccentricity*Math.cos(vEarth));
         double lEarth = vEarth + EARTH.varpi;
         double lambda;
         double k = rEarth*sin(lPrim-lEarth);
+
         if(innerPlanet.contains(this)){
             lambda = PI + lEarth + Math.atan2(rPrim*sin(lEarth-l), rEarth - rPrim*cos(lEarth-l));
-        }else{
+        }
+        else{
             lambda = lPrim+Math.atan2(k, rPrim-rEarth*cos(lPrim-lEarth));
         }
         double beta = Math.atan2(rPrim * tan(phi)*sin(lambda-lPrim), k);
@@ -84,6 +101,11 @@ public enum PlanetModel implements CelestialObjectModel<Planet> {
         return new Planet(this.frenchName, equatorialCoords,(float) theta, (float) m);
     }
 
+
+    /**
+     * Method createOuterPlanetSet returning the set of planets beyond Earth
+     * @return set (Set<PlanetModel>) : return the set of planets beyond Earth
+     */
     private static Set<PlanetModel> createOuterPlanetSet() {
         Set<PlanetModel> set = new HashSet<>();
         set.add(MARS);
@@ -94,6 +116,11 @@ public enum PlanetModel implements CelestialObjectModel<Planet> {
         return set;
     }
 
+
+    /**
+     * Method createOuterPlanetSet returning the set of planets closer from the Sun
+     * @return set (Set<PlanetModel>) : return the set of planets closer from the sun
+     */
     private static Set<PlanetModel> createInnerPlanetSet(){
         Set<PlanetModel> set = new HashSet<>();
         set.add(MERCURY);
