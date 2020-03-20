6a4093c18cd0a464a4e6737e8c03906712006d65
Clementee <61148897+Clementee@users.noreply.github.com>
2020-03-20 12:52:18 +0100

    Update SunModel.java

--- src/ch/epfl/rigel/astronomy/SunModel.java
+++ src/ch/epfl/rigel/astronomy/SunModel.java
@@ -3,9 +3,9 @@ package ch.epfl.rigel.astronomy;
 import ch.epfl.rigel.coordinates.EclipticCoordinates;
 import ch.epfl.rigel.coordinates.EclipticToEquatorialConversion;
 import ch.epfl.rigel.coordinates.EquatorialCoordinates;
+import ch.epfl.rigel.math.Angle;
 
-import static ch.epfl.rigel.math.Angle.TAU;
-import static ch.epfl.rigel.math.Angle.ofDeg;
+import static ch.epfl.rigel.math.Angle.*;
 import static java.lang.Math.*;
 
 /**
@@ -17,19 +17,18 @@ import static java.lang.Math.*;
 public enum SunModel implements CelestialObjectModel<Sun> {
     SUN;
 
-    private final static EclipticCoordinates J2010SunCoordinates = EclipticCoordinates.of(ofDeg(279.557208), ofDeg(283.112438));
+    private final static double LONSUN2010 = Angle.ofDeg(279.557208);
+    private final static double LONSUNPERIGEE = Angle.ofDeg(283.1124);
     private final static double J2010SunEarthExcentricity = 0.016705;
     private final static double THETA0 = ofDeg(0.533128);
-    private static double M0;
-    private static double lambda0;
 
     @Override
     public Sun at(double daysSinceJ2010, EclipticToEquatorialConversion eclipticToEquatorialConversion) {
-        M0 = TAU/365.242191 * daysSinceJ2010 + J2010SunCoordinates.lon() - J2010SunCoordinates.lat();
+        double M0 = TAU/365.242191 * daysSinceJ2010 +LONSUN2010 - LONSUNPERIGEE;
         double v0 = M0 + 2 * J2010SunEarthExcentricity *sin(M0);
-        lambda0 = v0 + J2010SunCoordinates.lat();
+        double lambda0 = v0 + LONSUNPERIGEE;
         double phi0 = 0;
-        double sunAngularSize = THETA0 * (1 + J2010SunEarthExcentricity * cos(v0)) / (1 - J2010SunEarthExcentricity);
+        double sunAngularSize = THETA0 * ((1 + J2010SunEarthExcentricity * cos(v0)) / (1 - J2010SunEarthExcentricity));
         EclipticCoordinates sunEclipticCoordinates = EclipticCoordinates.of(lambda0, phi0);
         EquatorialCoordinates sunEquatorialCoordinates = eclipticToEquatorialConversion.apply(sunEclipticCoordinates);
         return new Sun(sunEclipticCoordinates, sunEquatorialCoordinates, (float) sunAngularSize, (float) M0);
