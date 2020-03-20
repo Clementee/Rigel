ca6678ac7e1dc8b814c585177f6cbcc6df999be8
Clementee <61148897+Clementee@users.noreply.github.com>
2020-03-20 14:50:39 +0100

    Create MoonModelTest.java

---
+++ test/ch/epfl/rigel/astronomy/MoonModelTest.java
@@ -0,0 +1,19 @@
+package ch.epfl.astronomy;
+
+import ch.epfl.rigel.astronomy.MoonModel;
+import ch.epfl.rigel.coordinates.EclipticToEquatorialConversion;
+import org.junit.jupiter.api.Test;
+
+import java.time.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MoonModelTest {
+
+    @Test
+    void at() {
+        assertEquals(-0.20114171346014934,MoonModel.MOON.at(-2313, new EclipticToEquatorialConversion(ZonedDateTime.of(LocalDate.of(2003,  Month.SEPTEMBER, 1),LocalTime.of(0,0), ZoneOffset.UTC))).equatorialPos().dec());
+        assertEquals(14.211456457835897,MoonModel.MOON.at(-2313, new EclipticToEquatorialConversion(ZonedDateTime.of(LocalDate.of(2003,  Month.SEPTEMBER, 1), LocalTime.of(0,0), ZoneOffset.UTC))).equatorialPos().raHr());
+    }
+
+}
