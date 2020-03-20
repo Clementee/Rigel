a8012d7e87f98c471fb6beafbbaae12619cc7559
Clementee <61148897+Clementee@users.noreply.github.com>
2020-03-20 15:22:24 +0100

    Update MoonModelTest.java

--- test/ch/epfl/rigel/astronomy/MoonModelTest.java
+++ test/ch/epfl/rigel/astronomy/MoonModelTest.java
@@ -10,10 +10,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MoonModelTest {
 
+    public final static double EPSILON = 10e-9;
+
     @Test
     void at() {
-        assertEquals(-0.20114171346014934,MoonModel.MOON.at(-2313, new EclipticToEquatorialConversion(ZonedDateTime.of(LocalDate.of(2003,  Month.SEPTEMBER, 1),LocalTime.of(0,0), ZoneOffset.UTC))).equatorialPos().dec());
-        assertEquals(14.211456457835897,MoonModel.MOON.at(-2313, new EclipticToEquatorialConversion(ZonedDateTime.of(LocalDate.of(2003,  Month.SEPTEMBER, 1), LocalTime.of(0,0), ZoneOffset.UTC))).equatorialPos().raHr());
+        assertEquals(-0.20114171346014934,MoonModel.MOON.at(-2313, new EclipticToEquatorialConversion(ZonedDateTime.of(LocalDate.of(2003,  Month.SEPTEMBER, 1),LocalTime.of(0,0), ZoneOffset.UTC))).equatorialPos().dec(),EPSILON);
+        assertEquals(14.211456457835897,MoonModel.MOON.at(-2313, new EclipticToEquatorialConversion(ZonedDateTime.of(LocalDate.of(2003,  Month.SEPTEMBER, 1), LocalTime.of(0,0), ZoneOffset.UTC))).equatorialPos().raHr(),EPSILON);
     }
-
 }
