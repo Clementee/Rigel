98960392cc5bb0a429fb04fdcdf563ae586bb083
Blecoeur <baptiste.lecoeur@epfl.ch>
2020-03-23 15:35:25 +0100

    Corrigé étape 4 non fini

--- src/ch/epfl/rigel/coordinates/StereographicProjection.java
+++ src/ch/epfl/rigel/coordinates/StereographicProjection.java
@@ -11,7 +11,8 @@ public final class StereographicProjection implements Function<HorizontalCoordin
     private static double radius;
     private static double cosPhy;
     private static double sinPhy;
-
+    //
+    
     /**
      * StereographicProjection public constructor initializing some values
      *
