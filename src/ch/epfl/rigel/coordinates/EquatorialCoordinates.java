90a27b5b6528542c0ae8b46b07893b6e65055c32
Clementee <61148897+Clementee@users.noreply.github.com>
2020-03-29 16:05:53 +0200

    Update EquatorialCoordinates.java

--- src/ch/epfl/rigel/coordinates/EquatorialCoordinates.java
+++ src/ch/epfl/rigel/coordinates/EquatorialCoordinates.java
@@ -1,55 +1,55 @@
-    package ch.epfl.rigel.coordinates;
+package ch.epfl.rigel.coordinates;
 
-    import java.util.Locale;
+import java.util.Locale;
 
-    import ch.epfl.rigel.math.Angle;
-    import ch.epfl.rigel.math.ClosedInterval;
-    import ch.epfl.rigel.math.RightOpenInterval;
+import ch.epfl.rigel.math.Angle;
+import ch.epfl.rigel.math.ClosedInterval;
+import ch.epfl.rigel.math.RightOpenInterval;
 
-    import static ch.epfl.rigel.Preconditions.checkInInterval;
+import static ch.epfl.rigel.Preconditions.checkInInterval;
+
+/**
+ * An equatorial coordinate
+ *
+ * @author Baptiste Lecoeur (316223)
+ * @author Clement Sanh (311427)
+ */
+public final class EquatorialCoordinates extends SphericalCoordinates {
+
+    private final static RightOpenInterval RAINTERVAL = RightOpenInterval.of(0, Angle.TAU);
+    private final static ClosedInterval DECINTERVAL =  ClosedInterval.symmetric(Math.PI);
 
     /**
-     * An equatorial coordinate
-     *
-     * @author Baptiste Lecoeur (316223)
-     * @author Clement Sanh (311427)
+     * EquatorialCoordinates package-private constructor
+     * @param rightAscension  (double) : gives the longitude/right ascension of the position
+     * @param declination     (double) : gives the latitude/declination of the position
      */
-    public final class EquatorialCoordinates extends SphericalCoordinates {
+    EquatorialCoordinates(double rightAscension, double declination) { super(rightAscension, declination);}
 
-        private final static RightOpenInterval RAINTERVAL = RightOpenInterval.of(0, Angle.TAU);
-        private final static ClosedInterval DECINTERVAL =  ClosedInterval.symmetric(Math.PI);
-
-        /**
-         * EquatorialCoordinates package-private constructor
-         * @param rightAscension  (double) : gives the longitude/right ascension of the position
-         * @param declination     (double) : gives the latitude/declination of the position
-         */
-        EquatorialCoordinates(double rightAscension, double declination) { super(rightAscension, declination);}
-
-        /**
-         * Public method used to call the private constructor while throwing an exception if not working
-         * @param ra    (double) : gives the longitude/right ascension value in rad of the position
-         * @param dec   (double) : gives the latitude/declination value in rad of the position
-         *
-         * @return      call the constructor with the entered parameters or throw exception
-         */
-        public static EquatorialCoordinates of(double ra, double dec) {
-            return new EquatorialCoordinates(checkInInterval(RAINTERVAL,ra),checkInInterval(DECINTERVAL,dec));
-        }
-
-        public double ra() { return super.lon();}
-
-        public double raDeg() { return super.lonDeg();}
-
-        public double raHr() {
-            double ra = super.lon();
-            return Angle.toHr(ra);
-        }
-
-        public double dec() { return super.lat();}
-
-        public double decDeg() { return super.latDeg();}
-
-        @Override
-        public String toString() { return String.format(Locale.ROOT,"(ra=%.4fh, dec=%.4f°)",raHr(),decDeg()); }
+    /**
+     * Public method used to call the private constructor while throwing an exception if not working
+     * @param ra    (double) : gives the longitude/right ascension value in rad of the position
+     * @param dec   (double) : gives the latitude/declination value in rad of the position
+     *
+     * @return      call the constructor with the entered parameters or throw exception
+     */
+    public static EquatorialCoordinates of(double ra, double dec) {
+        return new EquatorialCoordinates(checkInInterval(RAINTERVAL,ra),checkInInterval(DECINTERVAL,dec));
     }
+
+    public double ra() { return super.lon();}
+
+    public double raDeg() { return super.lonDeg();}
+
+    public double raHr() {
+        double ra = super.lon();
+        return Angle.toHr(ra);
+    }
+
+    public double dec() { return super.lat();}
+
+    public double decDeg() { return super.latDeg();}
+
+    @Override
+    public String toString() { return String.format(Locale.ROOT,"(ra=%.4fh, dec=%.4f°)",raHr(),decDeg()); }
+}
