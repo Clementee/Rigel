308fde557d8db58a671080a1cddfb47648a1f298
Clementee <61148897+Clementee@users.noreply.github.com>
2020-03-29 14:24:04 +0200

    Update HygDatabaseLoader.java

--- src/ch/epfl/rigel/astronomy/HygDatabaseLoader.java
+++ src/ch/epfl/rigel/astronomy/HygDatabaseLoader.java
@@ -4,71 +4,99 @@ import ch.epfl.rigel.coordinates.EquatorialCoordinates;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.LinkedList;
 
 import static java.nio.charset.StandardCharsets.US_ASCII;
 
+
+/**
+ * A loader of stars from a database
+ *
+ * @author Baptiste Lecoeur (316223)
+ * @author Clement Sanh (311427)
+ */
 public enum HygDatabaseLoader implements StarCatalogue.Loader{
+
     INSTANCE;
 
+    /**
+     * HygDatabaseLoader method loading the stars from an inputStream into a builder
+     *
+     * @param inputStream   (InputStream) : gives the inputStream from where we take the stars
+     * @param builder       (StarCatalogue.builder) : gives the builder of the star catalogue we are initializing
+     * @throws IOException   :
+     */
     @Override
     public void load(InputStream inputStream, StarCatalogue.Builder builder) throws IOException {
+        
         ArrayList<String[]> inputTab = new ArrayList<>();
+        
         try (InputStreamReader inputStreamReader = new InputStreamReader(inputStream, US_ASCII)) {
             try (BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+                
                 String inputLine = bufferedReader.readLine();
+                
                 while (bufferedReader.ready()) {
                     inputLine = bufferedReader.readLine();
                     inputTab.add(inputLine.split(","));
                 }
 
                 for (String[] strings : inputTab) {
+                    
                     int hipparcosID;
                     String name;
                     double rarad, decrad;
                     float magnitude, colorID;
-                    String[] tab = strings;
-                    String valInt = tab[index.HIP.ordinal()];
+                    String valInt = strings[index.HIP.ordinal()];
+                    
                     if (!valInt.equals("")) {
                         hipparcosID = Integer.parseInt(valInt);
-                    } else {
+                    } 
+                    else {
                         hipparcosID = 0;
                     }
-                    valInt = tab[index.PROPER.ordinal()];
+                    
+                    valInt = strings[index.PROPER.ordinal()];
+                    
                     if (!valInt.equals("")) {
                         name = valInt;
-                    } else {
-                        valInt = tab[index.BAYER.ordinal()];
+                    } 
+                    else {
+                        valInt = strings[index.BAYER.ordinal()];
                         if (!valInt.equals("")) {
-                            name = valInt + tab[index.CON.ordinal()];
-                        } else {
-                            name = "?" + tab[index.CON.ordinal()];
+                            name = valInt + strings[index.CON.ordinal()];
+                        }
+                        else {
+                            name = "?" + strings[index.CON.ordinal()];
                         }
                     }
-                    rarad = Double.parseDouble(tab[index.RARAD.ordinal()]);
-                    decrad = Double.parseDouble(tab[index.DECRAD.ordinal()]);
-                    valInt = tab[index.MAG.ordinal()];
+                    
+                    rarad = Double.parseDouble(strings[index.RARAD.ordinal()]);
+                    decrad = Double.parseDouble(strings[index.DECRAD.ordinal()]);
+                    valInt = strings[index.MAG.ordinal()];
                     EquatorialCoordinates equatorialCoordinates = EquatorialCoordinates.of(rarad, decrad);
+                    
                     if (!valInt.equals("")) {
                         magnitude = Float.parseFloat(valInt);
                     } else {
                         magnitude = 0;
                     }
-                    valInt = tab[index.CI.ordinal()];
+                    valInt = strings[index.CI.ordinal()];
                     if (!valInt.equals("")) {
                         colorID = Float.parseFloat(valInt);
                     } else {
                         colorID = 0;
                     }
+                    
                     builder.addStar(new Star(hipparcosID, name, equatorialCoordinates, magnitude, colorID));
-                }
                 }
             }
         }
+    }
 
-
+    /**
+     * private enum of indexes for the loader
+     *
+     */
     private enum index{
         ID, HIP, HD, HR, GL, BF, PROPER, RA, DEC, DIST, PMRA, PMDEC,
         RV, MAG, ABSMAG, SPECT, CI, X, Y, Z, VX, VY, VZ,
