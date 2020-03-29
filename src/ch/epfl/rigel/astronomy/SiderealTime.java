8b18d880f0f96999b24f0f74cfe0db6cea90480e
Clementee <61148897+Clementee@users.noreply.github.com>
2020-03-29 16:17:44 +0200

    Update SiderealTime.java

--- src/ch/epfl/rigel/astronomy/SiderealTime.java
+++ src/ch/epfl/rigel/astronomy/SiderealTime.java
@@ -8,8 +8,8 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 
+import static ch.epfl.rigel.astronomy.Epoch.J2000;
 import static ch.epfl.rigel.math.Angle.*;
-//import ch.epfl.rigel.astronomy.Epoch;
 
 /**
  * A sidereal time
@@ -18,13 +18,12 @@ import static ch.epfl.rigel.math.Angle.*;
  * @author Clement Sanh (311427)
  */
 public final class SiderealTime {
-    
     private final static double MILLITOSECONDS = 1000;
     private final static double SECONDSTOHOURS = 3600;
     private final static double S1_COEFF = 1.002737909;
     private final static Polynomial S0POLY = Polynomial.of(0.000025862,2400.051336,6.697374558);
 
-   /**
+    /**
      * Sidereal Time empty private constructor
      */
     private SiderealTime(){}
@@ -36,26 +35,25 @@ public final class SiderealTime {
      * @return (double) : the Sidereal Greenwich time in radians(in the interval [0, PI[
      */
     public static double greenwich(ZonedDateTime when){
-        
         ZonedDateTime whenBis = when.withZoneSameInstant(ZoneId.of("UTC"));
-        double T =Epoch.J2000.julianCenturiesUntil(whenBis.truncatedTo(ChronoUnit.DAYS));
+        double T = J2000.julianCenturiesUntil(whenBis.truncatedTo(ChronoUnit.DAYS));
         double t = whenBis.truncatedTo(ChronoUnit.DAYS).until(whenBis, ChronoUnit.MILLIS)/(MILLITOSECONDS*SECONDSTOHOURS);
         double S0 = S0POLY.at(T);
         double S1 = S1_COEFF*t;
         double Sg = S0+S1;
-        
+
         if(Sg<0){
             Sg=Sg % 24+24;
         }
         else{
-           Sg=Sg%24;
+            Sg=Sg%24;
         }
-        return Angle.normalizePositive(ofHr(Sg));
+        return ofHr(Sg);
     }
 
     /**
      * Method giving the local Sidereal time
-     * 
+     *
      * @param when (ZoneDateTime) : a date/hour couple with a time
      * @param where (Geographic Coordinates) : the position
      * @return (double) : the local Sidereal time
