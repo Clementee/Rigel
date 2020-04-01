729694fd19e429ed55cfdcf8d21cb9eecfb92cc8
Clementee <61148897+Clementee@users.noreply.github.com>
2020-04-01 16:05:12 +0200

    Update BlackBodyColor.java

--- src/ch/epfl/rigel/gui/BlackBodyColor.java
+++ src/ch/epfl/rigel/gui/BlackBodyColor.java
@@ -7,26 +7,21 @@ import java.io.*;
 import java.util.*;
 
 import static ch.epfl.rigel.Preconditions.checkInInterval;
-import static java.nio.charset.StandardCharsets.US_ASCII;
 
 public class BlackBodyColor {
 
     private static final String COLOR_CATALOGUE_NAME = "/bbr_color.txt";
-
-
-    private InputStream hygStream = getClass().getResourceAsStream(COLOR_CATALOGUE_NAME);
     private Map<Integer, Color> colorList = load();
 
 
-    public Color colorForTemperature(int temperatureKelv) {
+    public Color colorForTemperature(double temperatureKelv) {
         checkInInterval(ClosedInterval.of(1000, 40000), temperatureKelv);
-        int modTemp = temperatureKelv%100;
-        temperatureKelv =+ (100-modTemp);
-        return colorList.get(temperatureKelv);
+        int actTemperatureKelv = (int) Math.round(temperatureKelv/100)*100;
+        return colorList.get(actTemperatureKelv);
     }
 
     private Map<Integer,Color> load(){
-        try (InputStreamReader inputStreamReader = new InputStreamReader(hygStream, US_ASCII)){
+        try (FileReader inputStreamReader = new FileReader(COLOR_CATALOGUE_NAME)){
             try(BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
                 while(bufferedReader.ready()){
                     if(!(bufferedReader.readLine().startsWith("#")|| bufferedReader.readLine().substring(10, 15).equals(" 2deg"))){
