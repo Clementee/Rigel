eedf4b08f5cbbc20cb955ae73564be4e88555de2
Clementee <61148897+Clementee@users.noreply.github.com>
2020-04-03 15:28:50 +0200

    Update BlackBodyColor.java

--- src/ch/epfl/rigel/gui/BlackBodyColor.java
+++ src/ch/epfl/rigel/gui/BlackBodyColor.java
@@ -3,27 +3,44 @@ package ch.epfl.rigel.gui;
 import ch.epfl.rigel.math.ClosedInterval;
 import javafx.scene.paint.Color;
 
-import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 import static ch.epfl.rigel.Preconditions.checkInInterval;
 
+/**
+ * Black body color
+ *
+ * @author Baptiste Lecoeur (316223)
+ * @author Clement Sanh (311427)
+ */
 public class BlackBodyColor {
 
     private static final String COLOR_CATALOGUE_NAME = "/bbr_color.txt";
     private final static Map<Integer, Color> colorList = load();
 
-    private BlackBodyColor() throws UnsupportedOperationException {
-    }
+    /**
+     * BlackBodyColor private and empty constructor to make the class not instantiable
+     * @throws UnsupportedOperationException : 
+     */
+    private BlackBodyColor() throws UnsupportedOperationException {}
 
+    /**
+     * BlackBodyColor public method colorForTemperature returning the color linked to the temperature
+     * @param temperatureKelv     (double) : gives the temperature in Kelvin
+     * @return colorForTemperature (Color) : gives the color linked to the temperature
+     */
     public static Color colorForTemperature(double temperatureKelv) {
         checkInInterval(ClosedInterval.of(1000, 40000), temperatureKelv);
         final int actTemperatureKelv = (int) Math.round(temperatureKelv / 100) * 100;
         return colorList.get(actTemperatureKelv);
     }
 
+    /**
+     * BlackBodyColor private method load returning the map of color and integers
+     * @return colorMap (Map<Integer,Color>) : gives the map of integers and colors
+     */
     private static Map<Integer, Color> load() {
 
         Map<Integer, Color> colorMap = new HashMap<>();
