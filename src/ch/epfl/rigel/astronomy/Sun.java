95e61341673198d2cad0223ab346d5cfc9e87609
Clementee <61148897+Clementee@users.noreply.github.com>
2020-04-06 20:44:40 +0200

    Update Sun.java

--- src/ch/epfl/rigel/astronomy/Sun.java
+++ src/ch/epfl/rigel/astronomy/Sun.java
@@ -12,30 +12,45 @@ import java.util.Objects;
  * @author Clement Sanh (311427)
  */
 public final class Sun extends CelestialObject{
+    
     private static double meanAn;
     private static EclipticCoordinates eclipticPosition;
 
     /**
      * Sun package-private constructor returning a celestial object and more precisely the sun except if the parameters are not working
+     * 
      * @param eclipticPos     (EclipticCoordinates) : gives the ecliptic coordinates of the sun
      * @param equatorialPos   (EquatorialCoordinates) : gives the equatorial coordinates of the planet
      * @param angularSize     (float) : gives the angular size of the planet
      * @param meanAnomaly      (float) : gives the anomaly
      */
     public Sun(EclipticCoordinates eclipticPos, EquatorialCoordinates equatorialPos, float angularSize, float meanAnomaly){
+        
         super("Soleil", equatorialPos, angularSize, -26.7f);
+        
         Objects.requireNonNull(eclipticPos);
+        
         eclipticPosition = eclipticPos;
         meanAn = meanAnomaly;
     }
 
+    /**
+     * Public method returning the ecliptic position of the sun
+     *
+     * @return eclipticPositionAn (EclipticCoordinates) : return the ecliptic coordinates of the sun
+     */
     public EclipticCoordinates eclipticPos(){
+        
         return eclipticPosition;
     }
-
+    
+    /**
+     * Public method returning the anomaly
+     *
+     * @return meanAn (double) : return the anomaly
+     */
     public double meanAnomaly(){
+        
         return meanAn;
     }
-
-
 }
