661a71cdbe8b02bacccee5840cb61387d0301db1
Clementee <61148897+Clementee@users.noreply.github.com>
2020-04-06 18:52:09 +0200

    Update AsterismLoader.java

--- src/ch/epfl/rigel/astronomy/AsterismLoader.java
+++ src/ch/epfl/rigel/astronomy/AsterismLoader.java
@@ -10,7 +10,6 @@ import static java.nio.charset.StandardCharsets.US_ASCII;
 
 /**
  * A loader of asterism from a database
- *
  * @author Baptiste Lecoeur (316223)
  * @author Clement Sanh (311427)
  */
@@ -21,40 +20,51 @@ public enum AsterismLoader implements StarCatalogue.Loader {
     /**
      * AsterismLoader method loading the asterism from an inputStream into a builder
      *
-     * @param inputStream (InputStream) : gives the inputStream from where we take the asterism
-     * @param builder     (StarCatalogue.builder) : gives the builder of the star catalogue we are initializing
-     * @throws IOException :
+     * @param inputStream   (InputStream) : gives the inputStream from where we take the asterism
+     * @param builder       (StarCatalogue.builder) : gives the builder of the star catalogue we are initializing
+     * @throws IOException : throws the IOException
      */
     @Override
     public void load(InputStream inputStream, StarCatalogue.Builder builder) throws IOException {
-        InputStreamReader inputStreamReader = new InputStreamReader(inputStream, US_ASCII);
-        try (BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
-            List<Star> starList = builder.stars();
-            List<String[]> inputList = new ArrayList<>();
-            String line = bufferedReader.readLine();
-            while (line!=null) {
-                String[] inputLineTab = line.split(",");
-                inputList.add(inputLineTab);
-                line = bufferedReader.readLine();
-            }
 
-            Map<Integer, Star> asterismListed = new HashMap<>();
+        try (InputStreamReader inputStreamReader = new InputStreamReader(inputStream , US_ASCII)){
+            try(BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
 
-            for (Star starInter : starList) {
-                asterismListed.put(starInter.hipparcosId(), starInter);
-            }
-            for (String[] hipparsList : inputList) {
+                List<Star> starList = builder.stars();
+                List<String[]> inputList = new ArrayList<>();
+                
+                Map<Integer,Star> asterismListed = new HashMap<>();
 
-                List<Star> asterismList = new LinkedList<>();
-
-                for (String string : hipparsList) {
-                    if (Integer.parseInt(string) != 0) {
-                        asterismList.add(asterismListed.get(Integer.parseInt(string)));
-                    }
+                while(bufferedReader.ready()){
+                    
+                    String[] inputLineTab = bufferedReader
+                            .readLine()
+                            .split(",");
+                    
+                    inputList.add(inputLineTab);
                 }
-                builder.addAsterism(new Asterism(asterismList));
+
+                for(Star starInter : starList){
+                
+                    asterismListed.put(starInter.hipparcosId() , starInter);
+                }
+                
+                for (String[] hipparsList : inputList) {
+
+                    List<Star> asterismList = new LinkedList<>();
+
+                    for (String string : hipparsList) {
+
+                        if(Integer.parseInt(string) != 0){
+
+                            asterismList.
+                                    add(asterismListed.get(Integer.parseInt(string)));
+                        }
+                    }
+
+                    builder.addAsterism(new Asterism(asterismList));
+                }
             }
         }
     }
 }
-
