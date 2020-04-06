5d1aac27901226c73c7e25a0a346b501fca0fb4a
Clementee <61148897+Clementee@users.noreply.github.com>
2020-04-06 20:39:22 +0200

    Update SiderealTime.java

--- src/ch/epfl/rigel/astronomy/SiderealTime.java
+++ src/ch/epfl/rigel/astronomy/SiderealTime.java
@@ -18,9 +18,11 @@ import static ch.epfl.rigel.math.Angle.*;
  * @author Clement Sanh (311427)
  */
 public final class SiderealTime {
-    private final static double MILLITOSECONDS = 1000;
-    private final static double SECONDSTOHOURS = 3600;
+    
+    private final static double MILLI_TO_SECONDS = 1000;
+    private final static double SECONDS_TO_HOURS = 3600;
     private final static double S1_COEFF = 1.002737909;
+    
     private final static Polynomial S0POLY = Polynomial.of(0.000025862,2400.051336,6.697374558);
 
     /**
@@ -35,19 +37,27 @@ public final class SiderealTime {
      * @return (double) : the Sidereal Greenwich time in radians(in the interval [0, PI[
      */
     public static double greenwich(ZonedDateTime when){
+        
         ZonedDateTime whenBis = when.withZoneSameInstant(ZoneId.of("UTC"));
-        double T = J2000.julianCenturiesUntil(whenBis.truncatedTo(ChronoUnit.DAYS));
-        double t = whenBis.truncatedTo(ChronoUnit.DAYS).until(whenBis, ChronoUnit.MILLIS)/(MILLITOSECONDS*SECONDSTOHOURS);
+        
+        double T = J2000
+                .julianCenturiesUntil(whenBis.truncatedTo(ChronoUnit.DAYS));
+        double t = whenBis
+                .truncatedTo(ChronoUnit.DAYS)
+                .until(whenBis, ChronoUnit.MILLIS)/(MILLI_TO_SECONDS*SECONDS_TO_HOURS);
+        
         double S0 = S0POLY.at(T);
-        double S1 = S1_COEFF*t;
-        double Sg = S0+S1;
+        double S1 = S1_COEFF * t;
+        double Sg = S0 + S1;
 
-        if(Sg<0){
-            Sg=Sg % 24+24;
+        if(Sg < 0){
+            
+            Sg = Sg % 24 + 24;
         }
-        else{
-            Sg=Sg%24;
+        else { 
+            Sg = Sg % 24;
         }
+        
         return ofHr(Sg);
     }
 
@@ -59,8 +69,10 @@ public final class SiderealTime {
      * @return (double) : the local Sidereal time
      */
     public static double local(ZonedDateTime when, GeographicCoordinates where){
+        
         double Sg = greenwich(when);
         double lambda = ofDeg(where.lonDeg());
+        
         return Angle.normalizePositive(Sg + lambda);
     }
 }
