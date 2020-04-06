a2fbb7fef10dc1b9944d01b46324ba2cad627bad
Clementee <61148897+Clementee@users.noreply.github.com>
2020-04-06 20:11:32 +0200

    Update Planet.java

--- src/ch/epfl/rigel/astronomy/Planet.java
+++ src/ch/epfl/rigel/astronomy/Planet.java
@@ -12,13 +12,14 @@ public final class Planet extends CelestialObject{
 
     /**
      * Planet public constructor returning a celestial object and more precisely a planet
+     * 
      * @param name  (String) : gives the name of the planet
      * @param equatorialPos   (EquatorialCoordinates) : gives the equatorial coordinates of the planet
      * @param angularSize (float) : gives the angular size of the planet
      * @param magnitude (float) : gives the magnitude of the planet
      */
     public Planet(String name, EquatorialCoordinates equatorialPos, float angularSize, float magnitude) {
+        
         super(name, equatorialPos, angularSize, magnitude);
     }
 }
-
