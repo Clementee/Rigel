57f02a56027d2c1bbef179bd609447538b2aa7c6
Blecoeur <baptiste.lecoeur@epfl.ch>
2020-04-20 17:29:28 +0200

    Possible implémentation de Discrete et Continuous

--- src/ch/epfl/rigel/gui/TimeAccelerator.java
+++ src/ch/epfl/rigel/gui/TimeAccelerator.java
@@ -8,10 +8,20 @@ public interface TimeAccelerator{
     ZonedDateTime adjust(ZonedDateTime T0, long elapsedTime);
 
     static TimeAccelerator continuous(int alpha){
-        return (alpha)->;
+        return new TimeAccelerator() {
+            @Override
+            public ZonedDateTime adjust(ZonedDateTime T0, long elapsedTime) {
+                return T0.plusNanos(alpha*elapsedTime);
+            }
+        };
     }
     static TimeAccelerator discrete(int v, Duration S){
-        return null;
+        return new TimeAccelerator() {
+            @Override
+            public ZonedDateTime adjust(ZonedDateTime T0, long elapsedTime) {
+                return T0.plusNanos((long) Math.floor(v*elapsedTime)*S.getNano());
+            }
+        };
     }
 
 }
