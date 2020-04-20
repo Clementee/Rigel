b06bb971d4e2f0b8c1d3a3f3d7e7733a162b6951
Clementee <61148897+Clementee@users.noreply.github.com>
2020-04-20 17:44:18 +0200

    Update NamedTimeAccelerator.java

--- src/ch/epfl/rigel/gui/NamedTimeAccelerator.java
+++ src/ch/epfl/rigel/gui/NamedTimeAccelerator.java
@@ -1,35 +1,40 @@
 package ch.epfl.rigel.gui;
 
 import java.time.Duration;
-import java.time.ZonedDateTime;
-import java.time.temporal.TemporalAmount;
 
-import static ch.epfl.rigel.gui.TimeAccelerator.continuous;
-import static ch.epfl.rigel.gui.TimeAccelerator.discrete;
-
+/**
+ * Enumeration of named timeAccelerator
+ *
+ * @author Baptiste Lecoeur (316223)
+ * @author Clement Sanh (311427)
+ */
 public enum NamedTimeAccelerator {
-    TIMES_1("1×", continuous(1)), TIMES_30("30×", continuous(30)),
-    TIMES_300("300×", continuous(300)), TIMES_3000("3000×",continuous(3000)),
-    DAY("jour", discrete(60, Duration.ofDays(1))), SIDEREAL_DAYS("jour sidéral", discrete(60, Duration.ofSeconds(23*3600+56*60+4)));
-    //private static int SIDEREAL_DAYS_IN_SECONDS = 23*3600+56*60+4;
 
-    private String frenchName;
-    private TimeAccelerator accelerator;
+    TIMES_ONE("1x", TimeAccelerator.continuous(1)),
+    TIMES_30("30x",TimeAccelerator.continuous(30)),
+    TIMES_300("300x",TimeAccelerator.continuous(300)),
+    TIMES_3000("3000x", TimeAccelerator.continuous(3000)),
+    DAY("jour",TimeAccelerator.discrete(60, Duration.ofDays(1))),
+    SIDEREAL_DAY("jour sidéral",TimeAccelerator.discrete(60,Duration.ofSeconds(23*3600+54*60+4)));
+
+    private final String name;
+    private final TimeAccelerator accelerator;
+
     NamedTimeAccelerator(String name, TimeAccelerator accelerator){
-        frenchName = name;
+        this.name = name;
         this.accelerator = accelerator;
     }
 
-    public String getName(){
-        return frenchName;
+    public String getName(NamedTimeAccelerator namedTimeAccelerator){
+        return namedTimeAccelerator.name;
     }
 
-    public TimeAccelerator getAccelerator() {
-        return accelerator;
+    public TimeAccelerator getAccelerator(NamedTimeAccelerator namedTimeAccelerator){
+        return namedTimeAccelerator.accelerator;
     }
 
     @Override
-    public String toString() {
-        return frenchName;
+    public String toString(){
+        return name;
     }
 }
