b3984c7197a2faac9fbd7c06c95939d4e358db9e
Clementee <61148897+Clementee@users.noreply.github.com>
2020-04-20 22:22:44 +0200

    Update TimeAnimator.java

--- src/ch/epfl/rigel/gui/TimeAnimator.java
+++ src/ch/epfl/rigel/gui/TimeAnimator.java
@@ -2,10 +2,17 @@ package ch.epfl.rigel.gui;
 
 import javafx.animation.AnimationTimer;
 
+
 public final class TimeAnimator extends AnimationTimer {
+
+    private DateTimeBean instantObservation;
+
+    public TimeAnimator(DateTimeBean dateTimeBean){
+        instantObservation = dateTimeBean;
+    }
 
     @Override
     public void handle(long l) {
-        
+
     }
 }
