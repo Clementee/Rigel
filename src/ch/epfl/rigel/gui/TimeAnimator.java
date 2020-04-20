3565312ae79c2aa03a79830e710aa7c4983db110
Clementee <61148897+Clementee@users.noreply.github.com>
2020-04-20 21:22:13 +0200

    Create TimeAnimator.java

---
+++ src/ch/epfl/rigel/gui/TimeAnimator.java
@@ -0,0 +1,11 @@
+package ch.epfl.rigel.gui;
+
+import javafx.animation.AnimationTimer;
+
+public final class TimeAnimator extends AnimationTimer {
+
+    @Override
+    public void handle(long l) {
+        
+    }
+}
