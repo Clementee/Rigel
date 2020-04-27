c521c8260599f133bab2a1544bcb985f768a4953
Clementee <61148897+Clementee@users.noreply.github.com>
2020-04-27 14:09:25 +0200

    Create ViewingParametersBean.java

---
+++ src/ch/epfl/rigel/gui/ViewingParametersBean.java
@@ -0,0 +1,42 @@
+package ch.epfl.rigel.gui;
+
+import ch.epfl.rigel.coordinates.HorizontalCoordinates;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+
+/**
+ * A viewing parameters bean
+ *
+ * @author Baptiste Lecoeur (316223)
+ * @author Clement Sanh (311427)
+ */
+public class ViewingParametersBean {
+
+    private final ObjectProperty<Double> fieldOfViewDegProperty = new SimpleObjectProperty<>();
+    private final ObjectProperty<HorizontalCoordinates> centerProperty = new SimpleObjectProperty<>();
+
+
+    public ObjectProperty<Double> fieldOfViewDeg(){
+        return fieldOfViewDegProperty;
+    }
+
+    public void setFieldOfViewDegProperty(double fieldOfView){
+        fieldOfViewDegProperty.set(fieldOfView);
+    }
+
+    public Double getFieldOfViewDeg(){
+        return fieldOfViewDegProperty.get();
+    }
+
+    public ObjectProperty<HorizontalCoordinates> centerProperty(){
+        return centerProperty;
+    }
+
+    public void setCenter(HorizontalCoordinates centerCoords){
+        centerProperty.set(centerCoords);
+    }
+
+    public HorizontalCoordinates getCenter(){
+        return centerProperty.get();
+    }
+}
