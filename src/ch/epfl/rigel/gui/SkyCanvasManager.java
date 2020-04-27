952a809734e4be5fdbcf4b8f9af1e459f99762f1
Blecoeur <baptiste.lecoeur@epfl.ch>
2020-04-27 15:17:20 +0200

    Reorganisation du package xxx.gui

--- src/ch/epfl/rigel/gui/SkyCanvasManager.java
+++ src/ch/epfl/rigel/gui/SkyCanvasManager.java
@@ -9,12 +9,16 @@ import ch.epfl.rigel.coordinates.HorizontalCoordinates;
 import ch.epfl.rigel.coordinates.StereographicProjection;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.Node;
+import javafx.scene.canvas.Canvas;
 
 public class SkyCanvasManager {
 
     public ObjectProperty<Double> mouseAzDeg = new SimpleObjectProperty<>();
     public ObjectProperty<Double> mouseAltDeg = new SimpleObjectProperty<>();
-    public ObjectProperty<CelestialObject> objectUnderMouse = new SimpleObjectProperty<>();
+        public ObjectProperty<CelestialObject> objectUnderMouse = new SimpleObjectProperty<>();
+
+    private Canvas canvas = new Canvas()
 
     private ObjectProperty<StereographicProjection> projection = new SimpleObjectProperty<>();
     private ObjectProperty<CartesianCoordinates> planeToCanvas = new SimpleObjectProperty<>();
@@ -27,7 +31,17 @@ public class SkyCanvasManager {
         projection.set(new StereographicProjection(viewingParameters.getCenter()));
         GeographicCoordinates coordinates = GeographicCoordinates.ofDeg(observerLocation.getLonDeg(),observerLocation.getLatDeg());
         observedSky.set(new ObservedSky(dateTime.getZonedDateTime(), coordinates, projection.get(), catalogue));
-        
+    }
 
+    public Canvas canvas(){
+        Canvas cv = new Canvas();
+        cv.setOnMousePressed(evt->{
+            if(evt.isPrimaryButtonDown()){
+                canvas.requestFocus();
+            }
+        });
+        cv.setOnMouseMoved(evt->mousePosition.set(CartesianCoordinates.of(evt.getX(), evt.getY())));
+        cv.setOnScroll(evt->ev);
+    return cv;
     }
 }
