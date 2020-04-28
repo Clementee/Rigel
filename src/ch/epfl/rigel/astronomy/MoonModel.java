08985c8c10960d77bdb00302371b45000689c6ac
Clementee <61148897+Clementee@users.noreply.github.com>
2020-04-28 18:34:39 +0200

    Update MoonModel.java

--- src/ch/epfl/rigel/astronomy/MoonModel.java
+++ src/ch/epfl/rigel/astronomy/MoonModel.java
@@ -23,24 +23,26 @@ public enum MoonModel implements CelestialObjectModel<Moon> {
     private final static double l0 = ofDeg(91.929336);
     private final static double P0 = ofDeg(130.143076);
     private final static double N0 = ofDeg(291.682547);
-    private final static double i = ofDeg(5.145396);
-    private final static double e = 0.0549;
+    private final static double I = ofDeg(5.145396);
+    private final static double E = 0.0549;
     private final static double THETA0 = ofDeg(0.5181);
 
     private static double SUN_M;
     private static double SUN_LAMBDA;
+    private static double sinSunM = 0;
 
-    private final static double lCste = ofDeg(13.1763966);
-    private final static double MmCste = ofDeg(0.1114041);
-    private final static double EvCste = ofDeg(1.2739);
-    private final static double AeCste = ofDeg(0.1858);
-    private final static double A3Cste = ofDeg(0.37);
-    private final static double EcCste = ofDeg(6.2886);
-    private final static double A4Cste = ofDeg(0.214);
-    private final static double VCste = ofDeg(0.6583);
 
-    private final static double NCste = ofDeg(0.0529539);
-    private final static double NPrimCste = ofDeg(0.16);
+    private final static double L_CSTE = ofDeg(13.1763966);
+    private final static double MM_CSTE = ofDeg(0.1114041);
+    private final static double EV_CSTE = ofDeg(1.2739);
+    private final static double AE_CSTE = ofDeg(0.1858);
+    private final static double A3_CSTE = ofDeg(0.37);
+    private final static double EC_CSTE = ofDeg(6.2886);
+    private final static double A4_CSTE = ofDeg(0.214);
+    private final static double V_CSTE = ofDeg(0.6583);
+
+    private final static double N_CSTE = ofDeg(0.0529539);
+    private final static double N_PRIM_CSTE = ofDeg(0.16);
 
     private static double lPrimPrim;
     private static double MmPrim;
@@ -66,6 +68,8 @@ public enum MoonModel implements CelestialObjectModel<Moon> {
                 .at(daysSinceJ2010, eclipticToEquatorialConversion)
                 .meanAnomaly();
 
+        sinSunM = sin(SUN_M);
+
         lPrimPrim = MoonLongitudinalOrbit(daysSinceJ2010);
 
         EclipticCoordinates moonEclipticCoords = MoonEclipticCoordinates(daysSinceJ2010);
@@ -85,16 +89,16 @@ public enum MoonModel implements CelestialObjectModel<Moon> {
      */
     private double MoonLongitudinalOrbit(double daysSinceJ2010) {
 
-        double l = lCste * daysSinceJ2010 + l0;
-        double Mm = l - MmCste * daysSinceJ2010 - P0;
-        double Ev = EvCste * sin(2 * (l - SUN_LAMBDA) - Mm);
-        double Ae = AeCste * sin(SUN_M);
-        double A3 = A3Cste * sin(SUN_M);
+        double l = L_CSTE * daysSinceJ2010 + l0;
+        double Mm = l - MM_CSTE * daysSinceJ2010 - P0;
+        double Ev = EV_CSTE * sin(2 * (l - SUN_LAMBDA) - Mm);
+        double Ae = AE_CSTE * sinSunM;
+        double A3 = A3_CSTE * sinSunM;
         MmPrim = Mm + Ev - Ae - A3;
-        Ec = EcCste * sin(MmPrim);
-        double A4 = A4Cste * sin(2 * MmPrim);
+        Ec = EC_CSTE * sin(MmPrim);
+        double A4 = A4_CSTE * sin(2 * MmPrim);
         double lPrim = l + Ev + Ec - Ae + A4;
-        double V = VCste * sin(2 * (lPrim - SUN_LAMBDA));
+        double V = V_CSTE * sin(2 * (lPrim - SUN_LAMBDA));
         return lPrim + V;
     }
 
@@ -106,10 +110,11 @@ public enum MoonModel implements CelestialObjectModel<Moon> {
      */
     private EclipticCoordinates MoonEclipticCoordinates(double daysSinceJ2010) {
 
-        double N = N0 - NCste * daysSinceJ2010;
-        double NPrim = N - NPrimCste * sin(SUN_M);
-        double LAMBDAm = atan2((sin(lPrimPrim - NPrim) * cos(i)), (cos(lPrimPrim - NPrim))) + NPrim;
-        double BETAm = asin(sin(lPrimPrim - NPrim) * sin(i));
+        double N = N0 - N_CSTE * daysSinceJ2010;
+        double NPrim = N - N_PRIM_CSTE * sinSunM;
+        final double sinSub = sin(lPrimPrim-NPrim);
+        double LAMBDAm = atan2((sinSub * cos(I)), (cos(lPrimPrim - NPrim))) + NPrim;
+        double BETAm = asin(sinSub * sin(I));
 
         return EclipticCoordinates
                 .of(normalizePositive(LAMBDAm), BETAm);
@@ -131,7 +136,7 @@ public enum MoonModel implements CelestialObjectModel<Moon> {
      */
     private double MoonAngularSize() {
 
-        double rho = (1 - e * e) / (1 + e * cos(MmPrim + Ec));
+        double rho = (1 - E * E) / (1 + E * cos(MmPrim + Ec));
 
         return THETA0 / rho;
     }
