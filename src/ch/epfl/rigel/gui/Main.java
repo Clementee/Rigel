d39cec35520974f248f3cfe0aa8453584f16d622
Clementee <61148897+Clementee@users.noreply.github.com>
2020-05-04 18:30:27 +0200

    Update Main.java

--- src/ch/epfl/rigel/gui/Main.java
+++ src/ch/epfl/rigel/gui/Main.java
@@ -1,6 +1,11 @@
 package ch.epfl.rigel.gui;
 
+import ch.epfl.rigel.coordinates.HorizontalCoordinates;
 import javafx.application.Application;
+import javafx.geometry.Orientation;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -20,7 +25,7 @@ public class Main extends Application {
         Rigel = stage;
 
         Pane skyView = new Pane();
-        HBox controlBar = new HBox();
+        HBox controlBar = controlBarCreator();
         Pane informationBar = new Pane();
 
         BorderPane mainPane = new BorderPane(skyView,controlBar,null, informationBar,null);
@@ -28,4 +33,25 @@ public class Main extends Application {
         mainPane.setMinHeight(600);
     }
 
+    private HBox controlBarCreator(){
+
+        HBox observationPos = new HBox();
+        HBox observationInstant = new HBox();
+        HBox timeAdvancement = new HBox();
+
+        Separator separator = new Separator(Orientation.VERTICAL);
+
+        Label longitude = new Label("Longitude (°) :");
+        longitude.setStyle("-fx-spacing: inherit; -fx-alignment: baseline-left;");
+        Label latitude = new Label("Latitude (°) :");
+        latitude.setStyle("-fx-spacing: inherit; -fx-alignment: baseline-left;");
+
+        TextField actualLon = new TextField();
+        actualLon.setStyle("-fx-pref-width: 60;\n" + "-fx-alignment: baseline-right;");
+
+        HBox controlBar = new HBox(separator,observationPos,observationInstant,timeAdvancement);
+
+        controlBar.setStyle("-fx-spacing: 4; -fx-padding: 4;");
+        return controlBar;
+    }
 }
