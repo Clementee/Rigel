78b85202903f1c7a3d279b629d841f3fa0bc9527
Clementee <61148897+Clementee@users.noreply.github.com>
2020-05-04 17:47:21 +0200

    Update Main.java

--- src/ch/epfl/rigel/gui/Main.java
+++ src/ch/epfl/rigel/gui/Main.java
@@ -2,11 +2,12 @@ package ch.epfl.rigel.gui;
 
 import javafx.application.Application;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    
+
     private Stage Rigel;
 
     public void main(){
@@ -15,13 +16,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        
+
         Rigel = stage;
 
         Pane skyView = new Pane();
-        Pane controlBar = new Pane();
+        HBox controlBar = new HBox();
         Pane informationBar = new Pane();
 
         BorderPane mainPane = new BorderPane(skyView,controlBar,null, informationBar,null);
+        mainPane.setMinWidth(800);
+        mainPane.setMinHeight(600);
     }
+
 }
